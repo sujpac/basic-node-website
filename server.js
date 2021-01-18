71a1e2c969b91a2acc038341867e8c17f85c9fa0
sujpac <sujit.pack@gmail.com>
2021-01-18 14:28:25 -0800

    move routing & responses out of server.py, add image upload

--- server.js
+++ server.js
@@ -3,53 +3,12 @@ var url = require("url");
 
 function start(route, handle) {
   function onRequest(request, response) {
-    var postData = "";
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " recieved.");
-
-    request.setEncoding("utf8");
-
-    request.addListener("data", function(postDataChunk) {
-      postData += postDataChunk;
-      console.log("Received POST data chunk '" +
-      postDataChunk + "'.");
-    });
-    request.addListener("end", function() {
-      route(handle, pathname, response, postData);
-    });
+    route(handle, pathname, response, request);
   }
-
-  var formidable = require('formidable'),
-      http = require('http'),
-      sys = require('util');
-
-  http.createServer(function(req, res) {
-    onRequest(req, res);
-
-    if (req.url == '/upload' && req.method.toLowerCase() == 'post') {
-      // parse a file upload
-      var form = new formidable.IncomingForm();
-      form.parse(req, function(err, fields, files) {
-        res.writeHead(200, {'content-type': 'text/plain'});
-        res.write('received upload:\n\n');
-        res.end(sys.inspect({fields: fields, files: files}));
-      });
-      return;
-    }
-
-    if (req.url == '/' || req.url == '/start') {
-      // show a file upload form
-      res.writeHead(200, {'content-type': 'text/html'});
-      res.end(
-        '<form action="/upload" enctype="multipart/form-data" '+
-        'method="post">'+
-        '<input type="text" name="title"><br>'+
-        '<input type="file" name="upload" multiple="multiple"><br>'+
-        '<input type="submit" value="Upload">'+
-        '</form>'
-      );
-    }
-  }).listen(8888);
+  http.createServer(onRequest).listen(8888);
+  console.log("Server has started.");
 }
 
 exports.start = start;
